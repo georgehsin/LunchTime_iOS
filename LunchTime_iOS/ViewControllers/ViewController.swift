198382eb324110aabb48b89f8368b7a4921214c6
George Hsin <georgehsin@ucla.edu>
2017-11-08 23:26:48 -0800

    First commit, added FB login

---
+++ LunchTime_iOS/ViewControllers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LunchTime_iOS
+//
+//  Created by George Hsin on 11/7/17.
+//  Copyright © 2017 georgehsin. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x:16, y:50, width: view.frame.width - 32, height:50)
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("Successfully logged in with Facebook...")
+    }
+
+
+}
+
