55a6ec78ad3c4088688afd65748093098b88956f
George Hsin <georgehsin@ucla.edu>
2017-11-16 23:36:27 -0800

    FEATURE: App flow laid out. CreateEvent storyboard and flow laid out. Removed Slide out menu

---
+++ LunchTime_iOS/Controllers/CreateEventViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateEventViewController.swift
+//  LunchTime_iOS
+//
+//  Created by George Hsin on 11/16/17.
+//  Copyright © 2017 georgehsin. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    
+    
+}
--- LunchTime_iOS/Controllers/HomeScreenViewController.swift
+++ LunchTime_iOS/Controllers/HomeScreenViewController.swift
@@ -8,75 +8,77 @@
 
 import UIKit
 
-class HomeScreenViewController: UIViewController {
+class HomeScreenViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupMenu()
         print("Arrived at HomeScreen")
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        navigationController?.setNavigationBarHidden(true, animated: false)
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewDidDisappear(animated)
-//        navigationController?.setNavigationBarHidden(false, animated: true)
-//    }
-    
-    let menuBackground = UIView()
-    var menu: UICollectionView!
-    
-    
-    func setupMenu() {
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        menu = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        menu!.backgroundColor = UIColor.white
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        self.tabBarController?.tabBar.isHidden = false
     }
 
-    
-    func showMenu() {
-        if let window = UIApplication.shared.keyWindow, let navController = self.navigationController {
-            menuBackground.backgroundColor = UIColor(white: 0, alpha: 0.5)
-            
-            menuBackground.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissMenu)))
-            
-            window.addSubview(menuBackground)
-            window.addSubview(menu)
-            let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
-            let navBarHeight = navController.navigationBar.frame.size.height
-            let height = window.frame.height - navBarHeight - statusBarHeight
-            let width: CGFloat = 150
-            menu.frame = CGRect(x: -width, y: navBarHeight + statusBarHeight, width: width, height: height)
-            
-            menuBackground.frame = window.frame
-            menuBackground.alpha = 0
-            
-            UIView.animate(withDuration: 0.5, animations: {
-                self.menuBackground.alpha = 1
-                self.menu.frame = CGRect(x: 0, y: navBarHeight + statusBarHeight, width: width, height: height)
-            })
-        }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    @objc func dismissMenu() {
-        print("dismissing")
-        UIView.animate(withDuration: 0.5) {
-            self.menuBackground.alpha = 0
-            if let window = UIApplication.shared.keyWindow, let navController = self.navigationController {
-                let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
-                let navBarHeight = navController.navigationBar.frame.size.height
-                let height = window.frame.height - navBarHeight - statusBarHeight
-                let width: CGFloat = 150
-                self.menu.frame = CGRect(x: -width, y: navBarHeight + statusBarHeight, width: width, height: height)
-            }
-        }
-    }
-    
-    @IBAction func sideMenuButtonPressed(_ sender: Any) {
-        showMenu()
+    @IBAction func cancelCreateEvent(unwindSegue: UIStoryboardSegue) {
+        //do something
     }
     
 }
+
+//MARK: Slide out menu
+//let menuBackground = UIView()
+//var menu: UICollectionView!
+//
+//
+//func setupMenu() {
+//    let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//    menu = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//    menu!.backgroundColor = UIColor.white
+//}
+//
+//
+//func showMenu() {
+//    if let window = UIApplication.shared.keyWindow, let navController = self.navigationController {
+//        menuBackground.backgroundColor = UIColor(white: 0, alpha: 0.5)
+//
+//        menuBackground.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissMenu)))
+//
+//        window.addSubview(menuBackground)
+//        window.addSubview(menu)
+//        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+//        let navBarHeight = navController.navigationBar.frame.size.height
+//        let height = window.frame.height - navBarHeight - statusBarHeight
+//        let width: CGFloat = 150
+//        menu.frame = CGRect(x: -width, y: navBarHeight + statusBarHeight, width: width, height: height)
+//
+//        menuBackground.frame = window.frame
+//        menuBackground.alpha = 0
+//
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.menuBackground.alpha = 1
+//            self.menu.frame = CGRect(x: 0, y: navBarHeight + statusBarHeight, width: width, height: height)
+//        })
+//    }
+//}
+//
+//@objc func dismissMenu() {
+//    print("dismissing")
+//    UIView.animate(withDuration: 0.5) {
+//        self.menuBackground.alpha = 0
+//        if let window = UIApplication.shared.keyWindow, let navController = self.navigationController {
+//            let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+//            let navBarHeight = navController.navigationBar.frame.size.height
+//            let height = window.frame.height - navBarHeight - statusBarHeight
+//            let width: CGFloat = 150
+//            self.menu.frame = CGRect(x: -width, y: navBarHeight + statusBarHeight, width: width, height: height)
+//        }
+//    }
+//}
+
