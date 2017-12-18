285ba45d49e9f50bbe4e4331c08548ee4af69ddf
George Hsin <georgehsin@ucla.edu>
2017-12-18 00:31:11 -0800

    FEATURE: When new event is created, event reference is created in every user invited

--- LunchTime_iOS/ViewModel/EventsViewModel.swift
+++ LunchTime_iOS/ViewModel/EventsViewModel.swift
@@ -14,38 +14,37 @@ class EventsViewModel {
     let uid = UserDefaults.standard.string(forKey: UserDefaults.UserDefaultKeys.userId.rawValue)
     
     func addEvent(date: Date, location: YLPBusiness, friends: [String:Friend]) {
-//        let friendsList = friends.flatMap({ (friend) -> [String:Any] in
-//            return [
-//                "uid": friend.uid,
-//                "username": friend.username,
-//                "attending": false
-//            ]
-//        })
-        let location = ["name": location.name, "yelpId": location.identifier]
-//        let invitedFriends = friends.map
-//        let friendsList = friends.map { (friend) -> [String:Any] in
-//            return [
-//                "uid": friend.uid,
-//                "username": friend.username,
-//                "attending": false
-//            ]
-//        }
+
+        let locationInfo = ["name": location.name, "yelpId": location.identifier]
         let eventFriends = friends.mapValues({ (friend) -> [String:Any] in
             return ["uid": friend.uid, "username": friend.username, "attending": false]
         })
-        print(eventFriends, location)
+        print(eventFriends, locationInfo)
         let eventData: [String: Any] = [
             "date": date,
-            "location": location,
+            "location": locationInfo,
             "friends": eventFriends
         ]
         
-        Firestore.firestore().collection("events").addDocument(data: eventData) { err in
+        let eventRef = Firestore.firestore().collection("events").addDocument(data: eventData) { err in
             if let err = err {
                 print("Error adding document: \(err)")
             } else {
                 print("Document added")
             }
+        }
+        
+        let event = [
+            "events": [
+                eventRef.documentID: [
+                    "date": date,
+                    "location": location.name
+                ]
+            ]
+        ]
+        Firestore.firestore().collection("users").document(uid!).setData(event, options: SetOptions.merge())
+        friends.forEach { (key, value) in
+            Firestore.firestore().collection("users").document(key).setData(event, options: SetOptions.merge())
         }
     }
 }
