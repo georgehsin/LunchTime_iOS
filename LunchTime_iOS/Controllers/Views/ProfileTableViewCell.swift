dfd69aaab21522a92679796a9e93d217170a7398
George Hsin <georgehsin@ucla.edu>
2018-01-04 15:02:09 -0800

    FEATURE/CHANGE: created Nib for profileController and UI updates

---
+++ LunchTime_iOS/Controllers/Views/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  LunchTime_iOS
+//
+//  Created by George Hsin on 1/4/18.
+//  Copyright © 2018 georgehsin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
